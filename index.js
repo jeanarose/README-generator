285ece058b7dcbcaaf45a5f4b3edeb17b8a08527
Jeana Rose <jr.mathis608@gmail.com>
2021-01-07 16:23:24 -0500

    Added a .then function to the inquirer prompt which addes the users answers to a response.json file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+// const generateMarkdown = require(generateMarkdown);
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -15,59 +16,73 @@ const questions = [
   "What does the user need to know about contributing to the repo?",
 ];
 
-  inquirer.prompt([
+inquirer
+  .prompt([
     {
       type: "input",
       message: questions[0],
       name: "gitHubUsername",
     },
     {
-        type: "input",
-        message: questions[1],
-        name: "email",
-      },
-    {
-        type: "input",
-        message: questions[2],
-        name: "projectName",
+      type: "input",
+      message: questions[1],
+      name: "email",
     },
     {
-        type: "input",
-        message: questions[3],
-        name: "description",
+      type: "input",
+      message: questions[2],
+      name: "title",
     },
     {
-        type: "input",
-        message: questions[4],
-        name: "license",
+      type: "input",
+      message: questions[3],
+      name: "description",
     },
     {
-        type: "input",
-        message: questions[5],
-        name: "commandToInstallDependencies",
+      type: "input",
+      message: questions[4],
+      name: "license",
     },
     {
-        type: "input",
-        message: questions[6],
-        name: "commandToRunTests",
+      type: "input",
+      message: questions[5],
+      name: "commandToInstallDependencies",
     },
     {
-        type: "input",
-        message: questions[7],
-        name: "repoInformation",
+      type: "input",
+      message: questions[6],
+      name: "commandToRunTests",
     },
     {
-        type: "input",
-        message: questions[8],
-        name: "contributions",
+      type: "input",
+      message: questions[7],
+      name: "repoInformation",
     },
-  ]);
+    {
+      type: "input",
+      message: questions[8],
+      name: "contributions",
+    },
+  ])
+  .then((response) => {
+    console.log(response);
+    fs.appendFile("response.js", JSON.stringify(response), (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully wrote user info to file.");
+      }
+    });
+  });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,5 +16,4 @@ function generateMarkdown(data) {
 
 `;
 }
-
 module.exports = generateMarkdown;
